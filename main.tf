60e4cfc80bfe960c1738385f0686de4ff9197c52
Shokhista Mardieva <shokhistamardieva@Shokhistas-MacBook-Air.local>
2025-04-28 08:28:21 -0400

    adding some chnages

--- main.tf
+++ main.tf
@@ -21,9 +21,23 @@ provider "aws" {}
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 } 
-
 module "security-groups" {
   source  = "app.terraform.io/029DA-DevOps24/security-groups/aws"
-  version = "1.0.0"
+  version = "2.0.0"
   vpc_id = aws_vpc.main.id# insert required variables here
+  dynamic "ingress" = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks  = ["0.0.0.0/0"]
+      description = "HTTP"
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks  = ["10.0.0.0/16"]
+      description = "HTTPS"
+  ]
 }
